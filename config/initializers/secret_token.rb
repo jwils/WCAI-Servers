75471215a06066f27a3e3aa85fb2ed407c5ad09b
Josh Wilson <jwils@Joshs-MacBook-Air.local>
2012-10-11 23:56:19 -0700

    Empty App Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WCAI::Application.config.secret_token = '08c6b65085e23d671cfadb531d04f8c1af765253d3c568e73756dfc752f2f7487bb80292aab6ad9c097fcd8a2715e678aa340dca4891366613c1d122b0b03f45'
