41dead6f5080fb962984cf56c4fc5a6ff48a7404
Josh Wilson <jwils@seas.upenn.edu>
2013-01-16 19:03:57 -0500

    fixed a few minor bugs when iniviting researchers.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130111210905) do
+ActiveRecord::Schema.define(:version => 20130113235258) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(:version => 20130111210905) do
     t.integer  "failed_attempts",                      :default => 0
     t.string   "unlock_token"
     t.datetime "locked_at"
+    t.string   "institution"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
