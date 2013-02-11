f5af87328acfe4549e0d77c8ac3c3f29d18b2364
Josh Wilson <jwils@seas.upenn.edu>
2013-02-11 03:03:12 -0500

    Small design changes suggested by Ben

--- app/controllers/passthrough_controller.rb
+++ app/controllers/passthrough_controller.rb
@@ -4,7 +4,7 @@ class PassthroughController < ApplicationController
 		if current_user.nil?
 			redirect_to new_user_session_path
 		elsif current_user.has_role? :admin
-			redirect_to home_page_path
+			redirect_to projects_path
 		else 
 			Project.all.each do |project|
 				if current_user.has_role? :researcher, project
