59c92d06f66e9afbdc648246edb61d7869ab4e96
Josh Wilson <jwils@seas.upenn.edu>
2013-02-17 17:59:16 -0500

    Fixed mailing problem

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ActionMailer::Base
   def added_to_project(user, project)
     @user = user
     @project = project
-    mail(:to => user.email, :subject => "You have been added to a research project by #{project.company.name}")
+    mail(:to => "#{user.name} <#{user.email}>", :subject => "You have been added to a research project by #{project.company.name}")
   end
 
   def send_email_to_list(current_user, users, subject, text)
