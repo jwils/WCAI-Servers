6328001696a529dd5bd20e7165aa8db3f29e9275
Josh Wilson <jwils@seas.upenn.edu>
2013-03-12 20:55:00 -0400

    Few more edits for change url path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 WCAI::Application.routes.draw do
   #Redirect since we changed server location
-  match 'projects/projects/:path' => redirect('projects/:path'), :path => /.+/
+  match 'projects/projects/:path' => 'passthrough#reroute_from_projects', :path => /.*/
 
   resources :timesheets
   match 'timesheets/:id/approve' => 'timesheets#approve', :as => :approve_timesheet
