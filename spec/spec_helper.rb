ceed01cbc482c39a73caf32954db1ed3e7420c5c
Josh Wilson <jwils@seas.upenn.edu>
2013-03-19 21:29:41 -0400

    Added email helper to specfile

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,16 +3,19 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-require 'capybara/rspec'j
+require 'capybara/rspec'
+require 'email_spec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
-  config.mock_with :rspec
-  config.use_transactional_fixtures = true
   config.include(MailerMacros)
+  config.include(EmailSpec::Helpers)
+  config.include(EmailSpec::Matchers)
+
+  config.mock_with :rspec
   
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
@@ -26,6 +29,8 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
+
+  config.use_transactional_fixtures = true
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
