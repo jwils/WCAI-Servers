be27bc7761eba8cc42cfc9b14cc414d3c1545480
Josh Wilson <jwils@seas.upenn.edu>
2013-04-18 18:59:23 -0400

    ? doesnt work in a variable

--- app/models/ec2_file.rb
+++ app/models/ec2_file.rb
@@ -1,14 +1,18 @@
 class Ec2File < WCAIFile
-  attr_accessor :directory?
+  attr_accessor :directory
 
   def self.create(parent, cmd_line_string)
     parts = cmd_line_string.split(' ')
     file = Ec2File.new
-    file.directory? = parts[0][0] == 'd'
+    file.directory = parts[0][0] == 'd'
     file.size = parts[4].to_i
     file.path = parent + parts[8..-1].join(" ")
     file.path += '/' if file.directory?
     file
+  end
+
+  def directory?
+    directory
   end
 
   def local?
