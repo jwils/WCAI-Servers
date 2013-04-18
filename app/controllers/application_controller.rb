fffbf0da6f99d37639ce4b0e0f5a4e305f1882d9
Josh Wilson <jwils@seas.upenn.edu>
2013-04-18 14:16:07 -0400

    Replaced custom method with devise builtin.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   check_authorization :unless => :devise_controller?
 
-  before_filter :check_logged_in, :unless => :devise_controller?
-
-  private
-
-  def check_logged_in
-    if current_user.nil?
-      flash[:notice] = "Please login first"
-      redirect_to new_user_session_path
-    end
-  end
+  before_filter :authenticate_user!, :unless => :devise_controller?
 end
